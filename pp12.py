e0ef2dc6eb97dc289f673b899953e178439543cd
vedantpatil1888 <vedpatil1008427@gmail.com>
2026-08-20 10:16:25 +0530

    Delete pp12.py

--- pp12.py
+++
@@ -1,8 +0,0 @@
-user_sentence = input("Enter a sentence: ")
-words = user_sentence.split()
-if words:
-    longest_word = max(words, key=len)
-    print(f"The longest word is: '{longest_word}'")
-    print(f"Its length is: {len(longest_word)}")
-else:
-    print("You did not enter any words.")
