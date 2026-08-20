85d70855981e9e8fb86eb05c6822f9a6839c520a
vedantpatil1888 <vedpatil1008427@gmail.com>
2026-08-20 10:13:33 +0530

    Delete pp13.py

--- pp13.py
+++
@@ -1,9 +0,0 @@
-user_sentence = input("Enter a sentence: ")
-words = user_sentence.split()
-if words:
-    shortest_word = min(words, key=len)
-    
-    print(f"The shortest word is: '{shortest_word}'")
-    print(f"Its length is: {len(shortest_word)}")
-else:
-    print("You did not enter any words.")
