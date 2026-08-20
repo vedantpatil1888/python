427d5b89e0a33dc3db3f8ec74b5f814b9c380535
vedantpatil1888 <vedpatil1008427@gmail.com>
2026-08-20 10:17:25 +0530

    Delete pp9.py

--- pp9.py
+++
@@ -1,8 +0,0 @@
-user_string = input("Enter a string: ")
-if len(user_string) > 0:
-    first_char = user_string[0]
-    last_char = user_string[-1]
-    print("First character:", first_char)
-    print("Last character:", last_char)
-else:
-    print("The string is empty.")
