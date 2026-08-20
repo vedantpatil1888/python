65844b9536c79d7bc9fe4f6b222394b775efff1f
vedantpatil1888 <vedpatil1008427@gmail.com>
2026-08-20 10:14:53 +0530

    Delete pp10.py

--- pp10.py
+++
@@ -1,6 +0,0 @@
-user_string = input("Enter a string: ")
-print("\nCharacter -> ASCII Value")
-print("-" * 24)
-for char in user_string:
-    ascii_value = ord(char)
-    print(f"    '{char}'   ->    {ascii_value}")
