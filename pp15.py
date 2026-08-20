a7a7221de2022c57a02fe4834c586e8bbeb6d56b
vedantpatil1888 <vedpatil1008427@gmail.com>
2026-08-20 10:19:21 +0530

    Delete pp15.py

--- pp15.py
+++
@@ -1,9 +0,0 @@
-user_string = input("Enter a string: ")
-char_counts = {}
-for char in user_string:
-    char_counts[char] = char_counts.get(char, 0) + 1
-print("Duplicate characters:")
-for char, count in char_counts.items():
-    if count > 1:
-        print(f"'{char}' appears {count} times")
-
