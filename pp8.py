6e2405d1aa8543071c52fa6d648d9d2ff1d228d4
vedantpatil1888 <vedpatil1008427@gmail.com>
2026-08-20 10:17:43 +0530

    Delete pp8.py

--- pp8.py
+++
@@ -1,7 +0,0 @@
-user_string = input("Enter a string: ")
-target_char = input("Enter the character to count: ")
-if len(target_char) == 1:
-    occurrence_count = user_string.count(target_char)
-    print(f"The character '{target_char}' appears {occurrence_count} times.")
-else:
-    print("Please enter exactly one character.")
